64c3e42fb899031f12b5855fc180a69e09c12d47
vanshika2809 <vanshika.meh@gmail.com>
2019-09-05 00:21:37 +0530

    timer

--- dice.js
+++ dice.js
@@ -1,13 +1,42 @@
+var sec=20;
 var diceTotal=0;
 function rollDice(){
-	var die= document.getElementById("Die");
+	var die= document.getElementById("die");
 	var status= document.getElementById("status");
 	var d=Math.floor(Math.random() * 6) +1;
 	document.getElementById('imgd').src = 'dice-' + d + '.png';
 	document.getElementById('die').innerHTML=d;
 		status.innerHTML="you rolled "+d+".";
-	var diceTotal= diceTotal+d;
+	 diceTotal= diceTotal+d;
 	die.innerHTML=d;
-	document.getElementById('total').textContent=diceTotal;
+	document.getElementById('total').innerHTML=diceTotal;
 
+
+
+	if(sec<=0)
+	{
+		Button.disabled=true;
+		alert("Game Over")
+	}
+
+	if(sec==20)
+	{
+		time();
+	}
+
+}
+
+function time()
+{
+	if(sec<=0)
+	{
+		Button.disabled=true;
+		alert("Game Over")
+	}
+	else{
+		sec=sec-1;
+		document.getElementById("time").innerHTML=sec;
+	}
+
+	setTimeout(function(){time();},1000);
 }
